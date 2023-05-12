8d7c54f67dbbc0b16ef0c64c94ba182f23601a2c
Dimitris Spyropoulos <dimitris.spyropoulos90@gmail.com>
2023-05-12 21:24:00 +0300

    registration, login and navbar completed

--- server/server.js
+++ server/server.js
@@ -4,10 +4,12 @@ const freeRoutes = require("./routes/freeRoutes")
 const protectedRoutes = require("./routes/protectedRoutes")
 const verifyToken = require("./authorize")
 require("./connection")
+const cors = require('cors')
+
 
 const port = 8000;
 app.use(express.json())
-
+app.use(cors())
 app.use("/", freeRoutes)
 app.use("/auth", verifyToken, protectedRoutes)
 
