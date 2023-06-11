2e48aa548f2c2c6dd9957932d73eebf8528aeb7e
Dimitris Spyropoulos <dimitris.spyropoulos90@gmail.com>
2023-06-11 11:23:49 +0300

    render

--- client/src/Components/config.js
+++ client/src/Components/config.js
@@ -1,6 +1,6 @@
 const API_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:8000"
-    : "https://finl.onrender.com";
+    : "https://one1-6.onrender.com";
 
 export default API_URL;
